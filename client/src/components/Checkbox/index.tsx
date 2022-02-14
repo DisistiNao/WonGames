0bac1f9e4a1c3a86331d175a8b1a3b3d8eb95d39
DisistiNao <gabrielsaldanha373@gmail.com>
2022-02-14 16:55:27 -0300

    Add userEvent lib and onChange method to Checkbox

--- client/src/components/Checkbox/index.tsx
+++ client/src/components/Checkbox/index.tsx
@@ -1,24 +1,45 @@
+import { InputHTMLAttributes, useState } from 'react'
 import * as S from './styles'
 
 export type CheckboxProps = {
+  onCheck?: (status: boolean) => void
   label?: string
   labelFor?: string
   labelColors?: 'black' | 'white'
-}
+} & InputHTMLAttributes<HTMLInputElement>
 
 const Checkbox = ({
+  onCheck,
   label,
   labelFor = '',
   labelColors = 'white'
-}: CheckboxProps) => (
-  <S.Wrapper>
-    <S.Input id={labelFor} type="checkbox" />
-    {!!label && (
-      <S.Label htmlFor={labelFor} labelColors={labelColors}>
-        {label}
-      </S.Label>
-    )}
-  </S.Wrapper>
-)
+}: CheckboxProps) => {
+  const [checked, setChecked] = useState(false)
+
+  const onChange = () => {
+    const status = !checked
+    setChecked(status)
+
+    if (onCheck) {
+      onCheck(status)
+    }
+  }
+
+  return (
+    <S.Wrapper>
+      <S.Input
+        id={labelFor}
+        type="checkbox"
+        onChange={onChange}
+        checked={checked}
+      />
+      {!!label && (
+        <S.Label htmlFor={labelFor} labelColors={labelColors}>
+          {label}
+        </S.Label>
+      )}
+    </S.Wrapper>
+  )
+}
 
 export default Checkbox
