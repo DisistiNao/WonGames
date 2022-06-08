7c9c06eee4275e5bbedbb53e61c828e7e640b51b
DisistiNao <gabrielsaldanha373@gmail.com>
2022-06-08 18:17:15 -0300

    Create GameInfo and add your index and tests

---
+++ client/src/components/GameInfo/index.tsx
@@ -0,0 +1,37 @@
+import Heading from 'components/Heading'
+import Button from 'components/Button'
+import Ribbon from 'components/Ribbon'
+
+import { AddShoppingCart } from '@styled-icons/material-outlined/AddShoppingCart'
+import { FavoriteBorder } from '@styled-icons/material-outlined/FavoriteBorder'
+
+import * as S from './styles'
+
+export type GameInfoProps = {
+  title: string
+  description: string
+  price: string
+}
+
+const GameInfo = ({ title, description, price }: GameInfoProps) => (
+  <S.Wrapper>
+    <Heading color="black" lineBottom>
+      {title}
+    </Heading>
+
+    <S.Description>{description}</S.Description>
+
+    <Ribbon color="secondary">{`$${price}`}</Ribbon>
+
+    <S.ButtonsWrapper>
+      <Button icon={<AddShoppingCart />} size="large">
+        Add to cart
+      </Button>
+      <Button icon={<FavoriteBorder />} size="large" minimal>
+        Wishlist
+      </Button>
+    </S.ButtonsWrapper>
+  </S.Wrapper>
+)
+
+export default GameInfo
