7a2ccd63be3aca5f51fe49599e7fafeaffb96733
DisistiNao <gabrielsaldanha373@gmail.com>
2022-06-14 17:11:53 -0300

    Add GameDetails storybook details

--- client/src/components/GameDetails/stories.tsx
+++ client/src/components/GameDetails/stories.tsx
@@ -1,17 +1,25 @@
 import { Story, Meta } from '@storybook/react'
 import GameDetails, { GameDetailsProps } from '.'
+import mockGame from './mock'
 
 export default {
   title: 'Game/GameDetails',
   component: GameDetails,
-  args: {
-    platforms: ['windows', 'linux', 'mac']
-  },
+  args: mockGame,
   argTypes: {
+    releaseDate: {
+      control: 'date'
+    },
     platforms: {
       control: {
         type: 'inline-check',
         options: ['windows', 'linux', 'mac']
+      }
+    },
+    genres: {
+      control: {
+        type: 'inline-check',
+        options: ['Role-playing', 'Narrative']
       }
     }
   },
--- client/src/components/GameDetails/test.tsx
+++ client/src/components/GameDetails/test.tsx
@@ -1,7 +1,7 @@
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
-import GameDetails, { GameDetailsProps } from '.'
+import GameDetails from '.'
 import mock from './mock'
 
 describe('<GameDetails />', () => {
