dac34462b65dd9964fb12d977f7d9a43c140309c
DisistiNao <gabrielsaldanha373@gmail.com>
2022-06-15 15:38:51 -0300

    Add close with ESC Modal on Gallery

--- client/src/components/Gallery/test.tsx
+++ client/src/components/Gallery/test.tsx
@@ -50,12 +50,26 @@ describe('<Gallery />', () => {
 
     // clicar para fechar o modal
     fireEvent.click(screen.getByRole('button', { name: /close modal/ }))
+
     expect(modal.getAttribute('aria-hidden')).toBe('true')
     expect(modal).toHaveStyle({ opacity: 0 })
   })
 
-  // // clicar no botão de fechar o menu e verificar se ele fechou
-  // fireEvent.click(screen.getByLabelText(/close menu/i))
-  // expect(modal.getAttribute('aria-hidden')).toBe('true')
-  // expect(modal).toHaveStyle({ opacity: 0 })
+  it('should handle close modal when ESC button is pressed', () => {
+    const { container } = renderWithTheme(<Gallery items={mock.slice(0, 2)} />)
+
+    // Selecionar o nosso modal
+    const modal = screen.getByLabelText('modal')
+
+    // clicar na modal e verificar se ela abriu
+    fireEvent.click(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 1/ })
+    )
+
+    // clicar para fechar o modal
+    fireEvent.keyUp(container, { key: 'Escape' })
+
+    expect(modal.getAttribute('aria-hidden')).toBe('true')
+    expect(modal).toHaveStyle({ opacity: 0 })
+  })
 })
