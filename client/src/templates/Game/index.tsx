0d2b3999514fd9109df79843cc67f7f991bb3403
DisistiNao <gabrielsaldanha373@gmail.com>
2022-06-16 12:33:47 -0300

    Add GameInfo to Game page

--- client/src/templates/Game/index.tsx
+++ client/src/templates/Game/index.tsx
@@ -1,14 +1,22 @@
 import Base from 'templates/Base'
+import GameInfo, { GameInfoProps } from 'components/GameInfo'
 
 import * as S from './styles'
 
 export type GameTemplanteProps = {
   cover: string
+  gameInfo: GameInfoProps
 }
 
-const Game = ({ cover }: GameTemplanteProps) => (
+const Game = ({ cover, gameInfo }: GameTemplanteProps) => (
   <Base>
     <S.Cover src={cover} role="image" aria-label="cover" />
+
+    <S.Main>
+      <S.SectionGameInfo>
+        <GameInfo {...gameInfo} />
+      </S.SectionGameInfo>
+    </S.Main>
   </Base>
 )
 
