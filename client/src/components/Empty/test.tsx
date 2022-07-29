93dd8dc9ce6255b613fba4dc8ea469bca06f3f7b
DisistiNao <gabrielsaldanha373@gmail.com>
2022-07-29 09:09:22 -0300

    Add Empty component

---
+++ client/src/components/Empty/test.tsx
@@ -0,0 +1,37 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Empty from '.'
+
+const props = {
+  title: 'A simple title',
+  description: 'A simple description'
+}
+
+describe('<Empty />', () => {
+  it('should render correctly', () => {
+    renderWithTheme(<Empty {...props} />)
+
+    expect(
+      screen.getByRole('image', {
+        name: /a gamer in a couch playing videogame/i
+      })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /a simple title/i })
+    ).toBeInTheDocument()
+    expect(screen.getByText(/a simple description/i)).toBeInTheDocument()
+    expect(
+      screen.queryByRole('link', { name: /go back to store/i })
+    ).not.toBeInTheDocument()
+  })
+
+  it('should render the button and the link goes home', () => {
+    renderWithTheme(<Empty {...props} hasLink />)
+
+    expect(
+      screen.getByRole('link', { name: /go back to store/i })
+    ).toHaveAttribute('href', '/')
+  })
+})
